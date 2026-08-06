1a000aab251a1ba924a29ce0556d3df1f4f6384f
Joona-123 <joona.laitala@outlook.com>
2026-08-06 17:20:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    jolloin EventSource sai seuraavilla latauksilla valmiiksi päättyneen
    vastauksen elävän yhteyden sijaan — yhteys näytti katkenneen pysyvästi. */
 
-var CACHE = "dobble-v61";
+var CACHE = "dobble-v60";
 var FILES = [
   "./", "./index.html", "./manifest.json", "./config.js",
   "./icon-192.png", "./icon-512.png",
